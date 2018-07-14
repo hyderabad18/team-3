008fc20a1c75083475f9cab1a02a2e902b7d2aa9
laxman2405 <laxman.madipadige@gmail.com>
2018-07-15 04:52:16 +0530

    Ongoing ang progress

---
+++ android/CFG_Youth4S/app/src/main/java/com/example/sai/cfg_youth4s/OngoingActivity.java
@@ -0,0 +1,92 @@
+package com.example.sai.cfg_youth4s;
+
+/**
+ * Created by sai on 14-07-2018.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sai on 14-07-2018.
+ */
+public class OngoingActivity extends AppCompatActivity {
+    ListView lv;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    ArrayList<EventDetails> arrayList;
+    ArrayList<EventDetails> NewarrayList = new ArrayList<>();
+    PendingAdapter arrayAdapter;
+    EventDetails uploadInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ongoing);
+
+        FontChangeCrawler fontChanger = new FontChangeCrawler(getAssets(), "open.ttf");
+        fontChanger.replaceFonts((ViewGroup)this.findViewById(android.R.id.content));
+
+        lv=(ListView)findViewById(R.id.listitem);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        String email = firebaseAuth.getCurrentUser().getEmail();
+        email=email.replaceAll("[^\\w\\s]","");
+
+        Toast.makeText(OngoingActivity.this,email, Toast.LENGTH_SHORT).show();
+
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference("Users").child(email).child("ongoing");
+
+        String userid = databaseReference.push().getKey();
+
+        arrayList = new ArrayList<>();
+        arrayAdapter = new PendingAdapter(this,uploadInfo);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds:dataSnapshot.getChildren())
+                {
+                    uploadInfo = ds.getValue(EventDetails.class);
+                    arrayList.add(new EventDetails(uploadInfo.getImageurl(),uploadInfo.getEventname(),uploadInfo.getEventlocation(),uploadInfo.getEventdate(),uploadInfo.getEventtime()));
+                }
+
+                for (EventDetails event : arrayList) {
+                    boolean isFound = false;
+                    // check if the event name exists in noRepeat
+                    for (EventDetails e : NewarrayList) {
+                        if ((e.getImageurl().equals(event.getImageurl()))) {
+                            isFound = true;
+                            break;
+                        }
+                    }
+                    if(!isFound) NewarrayList.add(event);
+                }
+                arrayAdapter = new PendingAdapter(getApplicationContext(),NewarrayList);
+                lv.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
+
