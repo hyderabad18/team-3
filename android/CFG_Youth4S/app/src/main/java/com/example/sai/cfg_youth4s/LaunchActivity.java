c524fa79c5427390cd45ac8b1ca4b3346c059fc2
ganeshgottipati <ganeshgottipati6@gmail.com>
2018-07-14 16:15:53 +0530

    Login_Signup

---
+++ android/CFG_Youth4S/app/src/main/java/com/example/sai/cfg_youth4s/LaunchActivity.java
@@ -0,0 +1,99 @@
+package com.example.sai.cfg_youth4s;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LaunchActivity extends AppCompatActivity {
+    private Button signin,regiser;
+    private FirebaseAuth firebaseAuth;
+    private EditText email,pwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        FontChangeCrawler fontChanger = new FontChangeCrawler(getAssets(), "open.ttf");
+        fontChanger.replaceFonts((ViewGroup)this.findViewById(android.R.id.content));
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null)
+        {
+            startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+        }
+
+        signin = (Button)findViewById(R.id.sign_in_button);
+        regiser = (Button)findViewById(R.id.sign_up_button);
+        email = (EditText)findViewById(R.id.email);
+        pwd = (EditText)findViewById(R.id.password);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LaunchActivity.this,LoginActivity.class));
+            }
+        });
+
+        regiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String ed1 = email.getText().toString().trim();
+                final String password = pwd.getText().toString().trim();
+
+                if (TextUtils.isEmpty(ed1)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(LaunchActivity.this,ed1, Toast.LENGTH_SHORT).show();
+
+                //create user
+                firebaseAuth.createUserWithEmailAndPassword(ed1, password)
+                        .addOnCompleteListener(LaunchActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(LaunchActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(LaunchActivity.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+//                                    Bundle b = new Bundle();
+//                                    b.putString("Username", ed1);
+//                                    intent.putExtras(b);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+
+            }
+        });
+
+    }
+}
